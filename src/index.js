146acb1ea398d2f96fe447839963d4f55c72c27a
weronika-fed <nikafed3@gmail.com>
2021-08-29 23:58:55 +0300

    feat: implement snake sort

---
+++ src/index.js
@@ -0,0 +1,24 @@
+
+
+
+module.exports = function towelSort (matrix) {
+    let result=[];
+    if (matrix === undefined){
+        return [];
+    }
+    for (let i = 0; i< matrix.length; i++){
+        
+        for (let j = 0; j < matrix[i].length; j++){
+            
+            let columnIndex = i % 2 === 0 ? j : (matrix[i].length - j - 1);
+            result.push(matrix[i][columnIndex]);
+            
+        }
+    } 
+    return result;
+ 
+} 
+
+
+
+
